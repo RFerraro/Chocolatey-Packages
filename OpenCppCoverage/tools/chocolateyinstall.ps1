111c2310a7c4e123104ec54e93218c08efbb64f7
Ryan Ferraro <jurianknight@gmail.com>
2016-08-19 12:48:27 -0700

    Initial version of the OpenCppCoverage package.

---
+++ OpenCppCoverage/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'OpenCppCoverage'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'EXE'
+  url           = 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=opencppcoverage&DownloadId=1594913&FileTime=131138685087100000&Build=21031'
+  url64bit      = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=opencppcoverage&DownloadId=1594912&FileTime=131138681432830000&Build=21031'
+  softwareName  = 'OpenCppCoverage*'
+
+  checksum      = '980DE3A272BB2AE325DAA3331319BC45854CBFD768EE1D7474FAA731656EAC42D4D0CC2970B3C4F54F8ADD6488DA73C5D5F8165828E3F0707C1DE06226B8440F'
+  checksumType  = 'sha512'
+  checksum64    = '49D4B42D9AAD4942F60E0AD72EBF99B89205D3E56A1FD8941A69ED1FDE8B2CB8DD8B0DD44A7D7FC4495153C908020426A1E760F5648CC15362EF1393E09D862A'
+  checksumType64= 'sha512'
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ OpenCppCoverage/tools/chocolateyuninstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$uninstalled = $false
+
+#only verified on a 64bit install on a 64bit os
+$uninstallKey = "{74933D3C-7641-4FA4-840E-313A4D076D87}_is1"
+$systemUninstallPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$uninstallPath = Join-Path $systemUninstallPath $uninstallKey
+
+if(Test-Path $uninstallPath)
+{
+    $packageArgs = @{
+      packageName    = 'OpenCppCoverage'
+      fileType       = 'EXE'
+      silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+      validExitCodes = @(0)
+      file           = "$((Get-ItemProperty $uninstallPath).UninstallString)"
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+}
+else
+{
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
